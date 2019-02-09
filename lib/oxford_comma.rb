33582cb8d5d8549676789049c9b3259543377fab
Fillan Lee <fillanlee@aol.com>
2019-02-09 02:41:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,14 @@
 def oxford_comma(array)
   # if array[-2]
   #   name = ar
-# array[-1] = "and #{array[-1]}"
+# "
  array[0..array.length-1].join(", ")
+ 
+ if array.length == 2 
+   array[-1] = "and #{array[-1]}"
+  elsif array.length == 3
+   array[-1] = "and #{array[-1]}"
+ else
+   array[-1] = "and #{array[-1]}""
+ end
 end
