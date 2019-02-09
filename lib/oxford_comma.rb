1ee8f324710dae7bc5dafdbb0b3b61a4da5f175d
Fillan Lee <fillanlee@aol.com>
2019-02-09 02:46:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,9 +6,8 @@ def oxford_comma(array)
  
  if array.length <= 1 
    array[-1]
-  elsif array.length >= 1
+  elsif array.length == 2
    array[-1] = "and #{array[-1]}"
-   array[0..array.length-1].join(", ")
  else
    array[-1] = "and #{array[-1]}"
    array[0..array.length-1].join(", ")
